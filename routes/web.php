e2d5fed3d3f8f05c4a5ac869b43750f83af7ca2d
emmacq <emmanuelcuazitlquecholac@gmail.com>
2026-01-01 15:32:00 -0600

    Cambios en rutas web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Página principal
+Route::get('/', [PaginaController::class, 'main'])->name('main');
+
+// Página Acerca
+Route::get('/acerca', [PaginaController::class, 'acerca'])->name('acerca');
+
+// Página Contacto
+Route::get('/contacto', [PaginaController::class, 'contacto'])->name('contacto');
+
+// Ruta con parámetro
+Route::get('/usuario/{id}', [PaginaController::class, 'usuario'])->name('usuario');
